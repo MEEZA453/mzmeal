4b55c3958f2aec89b4ecc3ffa708a6fba1900212
Your Name <meejanursk@gmail.com>
2025-04-30 11:59:58 +0530

    df

--- server/config/postgres.js
+++ server/config/postgres.js
@@ -2,6 +2,13 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.POSTGRES_URI, {
   dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,  // This is important for secure connections on Render
+    },
+  },
 });
 
 module.exports = sequelize;
